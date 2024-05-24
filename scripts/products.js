baacc7f3749d9fe2737ced486debf8d76fd47e42
yury-poloshkov <1286600@gmail.com>
2024-05-24 03:01:48 +0300

    addition to cart completed

--- scripts/products.js
+++ scripts/products.js
@@ -11,22 +11,14 @@ async function fetchData(url) {
 const url = '../data/products.json';
 const productsDB = await fetchData(url);
 
-console.log(productsDB);
-
 const products = document.querySelector('.products');
-
-console.log(products);
-
-productsDB.forEach(element => {
-    console.log(element);
-});
 
 productsDB.forEach(product => {
     products.insertAdjacentHTML('beforeend', `
         <article class="product">
             <div class="product_photo">
                 <img src="${product.img}" alt="${product.name}" />
-                <button class="add_to_bascket">
+                <button class="add_to_bascket" value="${product.id}">
                     <img
                         class="bascket_logo"
                         src="./img/basket.svg"
@@ -44,3 +36,52 @@ productsDB.forEach(product => {
     `)
 });
 
+const cart = document.querySelector('.cart');
+
+products.addEventListener('click', (e) => {
+    let productID = null;
+    if ((e.target.matches(".add_to_bascket"))) {
+        productID = e.target.value;
+    } else if ((e.target.matches(".bascket_logo"))) {
+        productID = e.target.parentElement.value;
+    }
+    if (productID != null) {
+        cart.style.display = "flex";
+        const productInCart = document.querySelector(`.productID-${productID}`);
+        if (!productInCart) {
+            cart.insertAdjacentHTML('beforeend', `
+                <div class="cart__product-card productID-${productID}">
+                    <div class="cart__product-image">
+                        <img class="cart__product-img" src="${productsDB[productID].img}" alt="" srcset=""/>
+                    </div>
+                    <article class="cart_product-description product_card">
+                        <h2 class="cart__product-name">${productsDB[productID].name}</h2>
+                        <p class="cart__product-parametr">
+                            Price: <span class="cart__product-price">$${productsDB[productID].price}</span>
+                        </p>
+                        <p class="cart__product-parametr">
+                            Color: <span class="cart__product-color">${productsDB[productID].color}</span>
+                        </p>
+                        <p class="cart__product-parametr">
+                            Size: <span class="cart__product-size">${productsDB[productID].size}</span>
+                        </p>
+                        <p class="cart__product-parametr">
+                            Quantity: 
+                            <input class="cart__product-quantity" type="number" name="" id="" min="1" value="1"/>
+                        </p>
+                    </article>
+                    <button class="cart__product-delete">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                            <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                            <path
+                                d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
+                            />
+                        </svg>
+                    </button>
+                </div>
+            `)
+        } else {
+            ++productInCart.querySelector('.cart__product-quantity').value;
+        }
+    };
+});
